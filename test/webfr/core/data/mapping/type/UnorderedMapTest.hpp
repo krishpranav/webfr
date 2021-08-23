043ba6f5c5909841a6a2de97ba2257024a6a4fc8
krishpranav <krisna.pranav@gmail.com>
2021-08-23 08:56:22 +0530

    test: webfr core data mapping type UnorderedMapTest

---
+++ test/webfr/core/data/mapping/type/UnorderedMapTest.hpp
@@ -0,0 +1,18 @@
+#ifndef webfr_test_core_data_mapping_type_UnorderedMapTest_hpp
+#define webfr_test_core_data_mapping_type_UnorderedMapTest_hpp
+
+#include "webfr-test/UnitTest.hpp"
+
+namespace webfr { namespace test { namespace core { namespace data { namespace mapping { namespace  type {
+
+class UnorderedMapTest : public UnitTest{
+public:
+
+  UnorderedMapTest():UnitTest("TEST[core::data::mapping::type::UnorderedMapTest]"){}
+  void onRun() override;
+
+};
+
+}}}}}}
+
+#endif 
