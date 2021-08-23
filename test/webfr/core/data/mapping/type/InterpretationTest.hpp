b629fc5d8ba2fed45e8f9508f4efca834d2b1588
krishpranav <krisna.pranav@gmail.com>
2021-08-23 08:34:11 +0530

    test: webfr core data mapping type InterpretionTest

---
+++ test/webfr/core/data/mapping/type/InterpretationTest.hpp
@@ -0,0 +1,18 @@
+#ifndef webfr_test_core_data_mapping_type_InterpretationTest_hpp
+#define webfr_test_core_data_mapping_type_InterpretationTest_hpp
+
+#include "webfr-test/UnitTest.hpp"
+
+namespace webfr { namespace test { namespace core { namespace data { namespace mapping { namespace  type {
+
+class InterpretationTest : public UnitTest{
+public:
+
+  InterpretationTest():UnitTest("TEST[core::data::mapping::type::InterpretationTest]"){}
+  void onRun() override;
+
+};
+
+}}}}}}
+
+#endif 
