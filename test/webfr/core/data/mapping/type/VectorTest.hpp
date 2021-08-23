52fecca9e0b0167fa79abafac9b20d2724f8bef3
krishpranav <krisna.pranav@gmail.com>
2021-08-23 08:49:03 +0530

    test: webfr core data mapping type VectorTest

---
+++ test/webfr/core/data/mapping/type/VectorTest.hpp
@@ -0,0 +1,18 @@
+#ifndef webfr_test_core_data_mapping_type_VectorTest_hpp
+#define webfr_test_core_data_mapping_type_VectorTest_hpp
+
+#include "webfr-test/UnitTest.hpp"
+
+namespace webfr { namespace test { namespace core { namespace data { namespace mapping { namespace  type {
+
+class VectorTest : public UnitTest{
+public:
+
+  VectorTest():UnitTest("TEST[core::data::mapping::type::VectorTest]"){}
+  void onRun() override;
+
+};
+
+}}}}}}
+
+#endif 
